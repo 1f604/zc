0cd227b66b84cb8dd923ee171e3c214df9baba74
1f604 <1f604@users.noreply.github.com>
2017-07-02 14:25:14 +0100

    added game over message on any player disconnect, fixed code styling issues according to PEP guidelines using flake8

--- client.py
+++ client.py
@@ -1,46 +1,54 @@
 import pygame
-from pygame.locals import *
-from library import *
-import thread, threading, random, socket, sys, message
-pygame.init()
+from pygame.locals import QUIT, KEYDOWN, K_ESCAPE
+from library import (territories, input_queue, output_queue, player,
+                     load_image, filepath, sprites, info, screen, width,
+                     height)
+# import thread
+import threading
+import socket
+import sys
+import message
 
-def create_connection():
-###Establish connection to server
-  # create a socket
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  # connect to server
-  host = sys.argv[1] # server address
-  port = int(sys.argv[2]) # server port
-  s.connect((host, port))
-  return s
+pygame.init()
+font = pygame.font.Font(None, 36)
+text = font.render("Game over: A player has disconnected", True, (255, 0, 250))
+end = False
+
+
+def create_connection():  # Establish connection to server
+    # create a socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to server
+    host = sys.argv[1]  # server address
+    port = int(sys.argv[2])  # server port
+    s.connect((host, port))
+    return s
+
 
 class receive_commands(threading.Thread):
-  def __init__(self, socket):
-    threading.Thread.__init__(self)
-    self.socket = socket
-  def run(self):
-    while True:
-        try:
-          command = message.recv_message(self.socket)
-          if command != '':
-            command = eval(command)
-            input_queue.put(command)
-        except:
-          thread.interrupt_main()
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        while True:
+                command = message.recv_message(self.socket)
+                if command != '':
+                    command = eval(command)
+                    input_queue.put(command)
+
 
 class send_commands(threading.Thread):
-  def __init__(self, socket):
-    threading.Thread.__init__(self)
-    self.socket = socket
-  def run(self):
-    while True:
-        try:
-              command = output_queue.get()
-              command = str(command)
-              message.send_message(self.socket, command)
-        except:
-          thread.interrupt_main()
-            
+    def __init__(self, socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+
+    def run(self):
+        while True:
+                command = output_queue.get()
+                command = str(command)
+                message.send_message(self.socket, command)
+
 
 sock = create_connection()
 t1 = receive_commands(sock)
@@ -50,54 +58,54 @@ t2 = send_commands(sock)
 t2.daemon = True
 t2.start()
 
+
 def update_world(new_state):
-###Update the territories based on command
-  for territory in territories:
-    for command in new_state[1:len(new_state)]:
-      if command[0] == territory.name:
-        territory.owner = command[1]
-        territory.armies = command[2]
-        territory.set_fields()
+    # Update the territories based on command
+    for territory in territories:
+        for command in new_state[1:len(new_state)]:
+            if command[0] == territory.name:
+                territory.owner = command[1]
+                territory.armies = command[2]
+                territory.set_fields()
+
 
 def process_command():
-  size = input_queue.qsize()
-  if size > 0:
-    for i in range(size):
-      command = input_queue.get()
-      if command[0] == 'ID':
-        player.ID = command[1]
-      elif command[0] == 'world':
-        update_world(command)
-      elif command[0] == 'quota':
-        player.quota = command[1]
-      elif command[0] == 'end':
-          pygame.quit() 
-          sys.exit()
-        
+    size = input_queue.qsize()
+    if size > 0:
+        for i in range(size):
+            command = input_queue.get()
+            if command[0] == 'ID':
+                player.ID = command[1]
+            elif command[0] == 'world':
+                update_world(command)
+            elif command[0] == 'quota':
+                player.quota = command[1]
+            elif command[0] == 'end':
+                print "#####################received end command##############"
+                global end
+                end = True
+
 
 def main(screen):
-  ###
-  #background = pygame.Surface([width, height])
-  #background.fill([0, 0, 0]
-  ###Create background
-  bg_image= load_image(filepath + "classic_board.jpg")
-  screen.blit(bg_image, (0, 0))
-  pygame.display.flip()
-  ###
-  clock = pygame.time.Clock()
-  running = True  
-  ###Main loop
-  while running:
-    clock.tick(30)
-    process_command()
-    for event in pygame.event.get():
-      if event.type == QUIT:
-        pygame.quit() 
-        sys.exit()
-      if event.type == KEYDOWN:
-        if event.key == K_ESCAPE:
-          pygame.quit() 
-          sys.exit()
+    # Create background
+    bg_image = load_image(filepath + "classic_board.jpg")
+    screen.blit(bg_image, (0, 0))
+    pygame.display.flip()
+    ###
+    clock = pygame.time.Clock()
+    running = True
+    # Main loop
+    while running:
+        clock.tick(30)
+        process_command()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
 #        if event.key == K_F5:
 #          fullscreen = pygame.FULLSCREEN
 #          screen = pygame.display.set_mode((width, height), fullscreen)
@@ -110,14 +118,20 @@ def main(screen):
 #            player.troops = num
 #        if event.key == K_0:
 #          player.troops = 10
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        for territory in territories:
-          territory.move()
-    sprites.clear(screen, bg_image)
-    time = pygame.time.get_ticks()
-    sprites.update(time)
-    info.update(time)
-    rectlist = sprites.draw(screen)
-    pygame.display.update(rectlist)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for territory in territories:
+                    territory.move()
+        if not end:
+            sprites.clear(screen, bg_image)
+            time = pygame.time.get_ticks()
+            sprites.update(time)
+            info.update(time)
+            rectlist = sprites.draw(screen)
+            pygame.display.update(rectlist)
+        else:
+            screen.blit(text, ((width-text.get_width())//2,
+                        height-text.get_height()))
+            pygame.display.flip()
+
 
 main(screen)
